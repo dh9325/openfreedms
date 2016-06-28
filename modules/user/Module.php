bc5166157e3a786095d4a240c45d140b67e0ca64
dhUK <dariusz.huk@me.com>
2016-06-28 22:10:56 +0100

    Initial commit; basic application structure based on yii2-app-advanced template

---
+++ modules/user/Module.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\modules\user;
+
+class Module extends \yii\base\Module
+{
+
+    public $defaultRoute = 'auth/login';
+    public $layoutPath = '@app/modules/user/views/layouts';
+    public $layout = 'main';
+
+    public function init()
+    {
+        parent::init();
+
+        \Yii::$app->setHomeUrl('/user/dashboard');
+        \Yii::$app->user->loginUrl = '/user/auth/login';
+        \Yii::$app->errorHandler->errorAction = '/user/dashboard/error';
+    }
+
+}
