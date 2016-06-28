def6961ab75ff6c06a197014c6e317c2b15162ae
dhUK <dariusz.huk@me.com>
2016-06-28 22:10:56 +0100

    Initial commit; basic application structure based on yii2-app-advanced template

---
+++ common/config/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@modules', dirname(dirname(__DIR__)) . '/modules');
+Yii::setAlias('@user', dirname(dirname(__DIR__)) . '/modules/user');
+Yii::setAlias('@admin', dirname(dirname(__DIR__)) . '/modules/admin');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@webroot', dirname(dirname(__DIR__)) . '/public');
+Yii::setAlias('@app', dirname(dirname(__DIR__)));
